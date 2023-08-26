bd912b43b17c06084473796eda68c8ce1eb55f1a
Subhajit Das <sdsubhajitdas98@gmail.com>
2023-08-26 11:11:28 +0530

    Checkout page redirect added if user is pro

--- ui/src/pages/Checkout.js
+++ ui/src/pages/Checkout.js
@@ -1,10 +1,18 @@
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "../components/CheckoutForm";
+import useAuthentication from "../hooks/useAuthentication";
+import { Navigate } from "react-router-dom";
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
 
 export default function Checkout() {
+  const {
+    authentication: { user },
+  } = useAuthentication();
+
+  if (user.subscription.tier === "PRO") return <Navigate to="/" />;
+
   return (
     <Elements stripe={stripePromise}>
       <CheckoutForm />
